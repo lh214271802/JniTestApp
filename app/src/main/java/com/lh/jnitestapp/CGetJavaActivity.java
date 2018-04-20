e4c08721b02008fbbc03a4df0ce4c8f285a1df6b
lh214271802 <214271802@qq.com>
2018-04-20 18:29:40 +0800

    c调java

--- app/src/main/java/com/lh/jnitestapp/CGetJavaActivity.java
+++ app/src/main/java/com/lh/jnitestapp/CGetJavaActivity.java
@@ -4,16 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author liaohui
  * @date 2018/4/20
+ * <p>
+ * C调用java主要使用的是反射机制，步骤如下：
+ * 1.得到字节码
+ * 2.得到方法
+ * 3.实例化该类
+ * 4.调用方法
  */
-public class CGetJavaActivity extends AppCompatActivity {
+public class CGetJavaActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_call_java_layout);
+        findViewById(R.id.sample_text).setOnClickListener(this);
+        findViewById(R.id.sample_text1).setOnClickListener(this);
+        findViewById(R.id.sample_text2).setOnClickListener(this);
     }
 
 
@@ -22,8 +33,35 @@ public class CGetJavaActivity extends AppCompatActivity {
      */
     public native void callBackAdd();
 
+    /**
+     * 执行这个方法的时候，让C代码调用
+     */
+    public native void callBackHelloFromJava();
+
     public int add(int x, int y) {
         Log.e("add", "C 调用了我-->" + (x + y));
+        Toast.makeText(this,"C 调用了我-->"+(x+y),Toast.LENGTH_SHORT).show();
         return x + y;
+    }
+
+    public String helloFromJava() {
+
+        return "hello from java";
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sample_text:
+               callBackAdd();
+                break;
+            case R.id.sample_text1:
+
+                break;
+            case R.id.sample_text2:
+
+                break;
+        }
     }
 }
--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -41,11 +41,20 @@ Java_com_lh_jnitestapp_MainActivity_checkPwd(JNIEnv *env, jobject instance, jstr
     const char *pwd = env->GetStringUTFChars(pwd_, 0);
     char *originPwd = "123456";
     int result = strcmp(pwd, originPwd);
-    if (result==0) { return 200; }
+    if (result == 0) { return 200; }
     else { return 400; }
 }extern "C"
 JNIEXPORT void JNICALL
 Java_com_lh_jnitestapp_CGetJavaActivity_callBackAdd(JNIEnv *env, jobject instance) {
+    jclass pJclass = env->FindClass("com/lh/jnitestapp/CGetJavaActivity.java");
+    //第三个参数是方法签名--->cmd运行：javap -s 字节码对应的类，显示方法签名
+    jmethodID pJmethodID = env->GetMethodID(pJclass, "add", "(II)I");
+    jobject pJobject = env->AllocObject(pJclass);
+    env->CallIntMethod(pJobject,pJmethodID,99,35);
+}extern "C"
+JNIEXPORT void JNICALL
+Java_com_lh_jnitestapp_CGetJavaActivity_callBackHelloFromJava(JNIEnv *env, jobject instance) {
 
+    // TODO
 
 }
